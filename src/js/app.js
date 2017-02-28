6c1a2a91fc0788fe18095bbfbd504c2c830f04cb
Harry Cresswell <studio@harrycresswell.com>
2017-02-28 10:28:01 +0100

    added ajax to send form submission to Zapier
    Using Zapier web hooks. Once form has been submitted ajax then
    redirects user to success page

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
 // JS Goes here
+$('#myForm').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+        url:'https://hooks.zapier.com/hooks/catch/1707140/msf6zi/',
+        type:'post',
+        data:$('#myForm').serialize(),
+        success:function(){
+          // On submit, redirect to another page
+          window.location = "http://localhost:1313/success";
+        }
+    });
+});
