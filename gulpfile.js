2fe8c0ceccbc5787991dce4ca9ad0a30ec47931b
Harry Cresswell <studio@harrycresswell.com>
2017-03-15 21:59:45 +0000

    removed hashing on CSS and JS and added shell task for production

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,43 @@
 var gulp         = require('gulp');
     sass         = require('gulp-sass');
     autoprefixer = require('gulp-autoprefixer');
-    hash         = require('gulp-hash');
     del          = require('del');
+    shell        = require('gulp-shell')
 
 
 // Compile SCSS files to CSS
 gulp.task('scss', function () {
 
-    //Delete our old css files
+    // Delete our old css files
     del(['static/css/**/*'])
 
-    //compile hashed css files
+    // Compile hashed css files
     gulp.src('src/scss/**/*.scss')
         .pipe(sass({outputStyle : 'compressed'}))
         .pipe(autoprefixer({browsers : ['last 20 versions']}))
-        .pipe(hash()) // Add hashes to the files' names
         .pipe(gulp.dest('static/css')) // Write the renamed files
-        //Create a hash map
-        .pipe(hash.manifest('hash.json'))
-        //Put the map in the data directory
-        .pipe(gulp.dest('data/css'))
 });
 
-// Hash javascript
+// Pipe JS to static folder
 gulp.task('js', function () {
     del(['static/js/**/*'])
     gulp.src('src/js/**/*')
-        .pipe(hash())
         .pipe(gulp.dest('static/js'))
-        //Create a hash map
-        .pipe(hash.manifest('hash.json'))
-        //Put the map in the data directory
-        .pipe(gulp.dest('data/js'))
 });
 
-// pipe images
+// Pipe images to static folder
 gulp.task('images', function () {
     del(['static/images/**/*'])
     gulp.src('src/images/**/*')
         .pipe(gulp.dest('static/images'))
 });
 
-// pipe fonts
+// Pipe fonts to static folder
 gulp.task('fonts', function() {
     del(['static/fonts/**/*'])
     gulp.src(['src/fonts/**/*'])
       .pipe(gulp.dest('static/fonts'))
 });
-
 
 // Watch asset folder for changes
 gulp.task('watch', ['scss', 'images', 'js', 'fonts'], function () {
@@ -58,6 +47,15 @@ gulp.task('watch', ['scss', 'images', 'js', 'fonts'], function () {
     gulp.watch('src/fonts/**/*', ['fonts']);
 });
 
-
 // Set watch as default task
 gulp.task('default', ['watch']);
+
+// Run all tasks ready for production
+gulp.task('prod', ['scss', 'images', 'js', 'fonts']);
+
+// Building the site in 'production mode' ready for deployment
+gulp.task('build', shell.task([
+  'rm -rf public',
+  'gulp prod',
+  'hugo'
+]))
